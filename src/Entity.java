81ea850259eec782041597e7234414841d0ef04f
Alex <alexanderz1511@gmail.com>
2013-05-23 21:48:06 -0700

    Removed src file

--- src/Entity.java
+++
@@ -1,9 +0,0 @@
-import javafx.scene.Node;
-
-
-public abstract class Entity {
-	public Node node;
-	public float xPos;
-	public float yPos;
-	public abstract Node create();
-}
--- src/GameMap.java
+++
@@ -1,7 +0,0 @@
-
-public class GameMap {
-	public GameMap(float width, float height){
-		
-	}
-
-}
