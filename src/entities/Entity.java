2c37d369c9b7c1f45e9b08f57d292ba2ad422ca6
edward <Edward.k.liang@gmail.com>
2013-06-06 01:14:40 -0700

    I have no clue how to use the merge tool properly, so some things may be broken.

---
+++ src/entities/Entity.java
@@ -0,0 +1,156 @@
+package entities;
+
+import java.io.Serializable;
+
+import org.jbox2d.collision.shapes.Shape;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.Fixture;
+import org.jbox2d.dynamics.FixtureDef;
+
+import infrastructure.App;
+import infrastructure.FXJBox;
+import infrastructure.GameMap;
+import infrastructure.Parse;
+import infrastructure.Util;
+import javafx.scene.Node;
+
+public abstract class Entity implements FXJBox, Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 42L;
+	public Node node;
+	protected float xPos;
+	protected float yPos;
+	protected float width;
+	protected float height;
+	protected BodyDef bd;
+	protected FixtureDef fd;
+	protected Shape ps;
+	protected Body body;
+	protected Fixture fixture;
+
+	public GameMap map;
+
+	public Entity(float posX, float posY, float width, float height) {
+		this.xPos = posX;
+		this.yPos = posY;
+		this.width = width;
+		this.height = height;
+		create();
+
+	}
+
+	public Entity(float posX, float posY) {
+		this.xPos = posX;
+		this.yPos = posY;
+		create();
+	}
+
+	public Entity() {
+
+	}
+
+	protected void create() {
+		node = createNode();
+		bd = createBD();
+		ps = createShape();
+		fd = createFD();
+		node.setLayoutX(Util.toPPosX(xPos) - Util.toPWidth(width) / 2);
+		node.setLayoutY(Util.toPPosY(yPos) - Util.toPWidth(height) / 2);
+	}
+
+	protected abstract Node createNode();
+
+	protected abstract Shape createShape();
+
+	protected abstract BodyDef createBD();
+
+	protected abstract FixtureDef createFD();
+
+	public void addToMap(GameMap map) {
+		if (this.map != null) {
+			removeFromMap();
+		}
+		this.map = map;
+		body = map.getPhysics().createBody(bd);
+		fixture = body.createFixture(fd);
+		Body body = map.getPhysics().createBody(bd);
+		body.createFixture(fd);
+		node.setUserData(body);
+		map.addEntity(this);
+	}
+
+	@Override
+	public void setVisible(Boolean bool) {
+		if (bool == true) {
+			if (App.root.getChildren().contains(node) == true) {
+				App.root.getChildren().remove(node);
+				App.root.getChildren().add(node);
+			} else
+				App.root.getChildren().add(node);
+		} else if (App.root.getChildren().contains(node) == true)
+			App.root.getChildren().remove(node);
+	}
+
+	public void removeFromMap() {
+		map.removeEntity(this);
+		map.getPhysics().destroyBody(this.getBody());
+		setVisible(false);
+		map = null;
+	}
+
+	@Override
+	public synchronized void setLayoutX(float x) {
+		node.setLayoutX(x);
+	}
+
+	@Override
+	public synchronized void setLayoutY(float y) {
+		node.setLayoutY(y);
+	}
+
+	public synchronized Body getBody() {
+		return (Body) node.getUserData();
+	}
+
+	@Override
+	public synchronized Vec2 getPosition() {
+		return getBody().getPosition();
+	}
+
+	@Override
+	public synchronized Vec2 getPPosition() {
+		return new Vec2(Util.toPPosX(getPosition().x),
+				Util.toPPosY(getPosition().y));
+	}
+
+	@Override
+	public synchronized double getPWidth() {
+		return Util.toPWidth(width);
+	}
+
+	@Override
+	public synchronized double getPHeight() {
+		return Util.toPHeight(height);
+	}
+
+	@Override
+	public synchronized float getWidth() {
+		return width;
+	}
+
+	@Override
+	public synchronized float getHeight() {
+		return height;
+	}
+
+	@Override
+	public String toString() {
+		return "" + this.getClass() + Parse.delim + xPos + Parse.delim + yPos
+				+ Parse.delim + width + Parse.delim + height;
+	}
+
+}
---
+++ src/infrastructure/App.java
@@ -0,0 +1,134 @@
+package infrastructure;
+import inputManagers.DefaultKeys;
+import inputManagers.DevModeKeys;
+import inputManagers.DevMouse;
+import inputManagers.FlyingKeys;
+import inputManagers.InertialKeys;
+import inputManagers.KeyManager;
+import inputManagers.MouseManager;
+import inputManagers.PixelEscapeKeys;
+import javafx.scene.control.*;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Cursor;
+import javafx.scene.Group;
+import javafx.scene.ImageCursor;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javafx.stage.Popup;
+import javafx.scene.text.Text;
+import org.jbox2d.collision.shapes.PolygonShape;
+
+import java.io.IOException;
+
+import org.jbox2d.common.Vec2;
+
+import entities.BouncyBall;
+import entities.Creature;
+import entities.PopupText;
+public class App extends Application {
+	public static GameWorld game;
+	public static Camera camera;
+	public static ShapeMaker shaker;
+	public static Group root;
+	public static Scene scene;
+	public static Stage pS;
+
+	public static void main(String[] args) throws IOException {
+		launch(args);
+	}
+
+	@Override
+	public void start(final Stage primaryStage) throws IOException {
+		pS = primaryStage;
+		primaryStage.setTitle("Dreamscape");
+		primaryStage.setFullScreen(false);
+		primaryStage.setResizable(false);
+
+		root = new Group();
+		scene = new Scene(root, Util.WIDTH, Util.HEIGHT);
+
+		game = new GameWorld();
+
+		scene.setCursor(Cursor.CROSSHAIR);
+		camera = new Camera();
+		shaker = new ShapeMaker();
+		DevModeKeys keyManager = new DevModeKeys();
+		MouseManager mouse = new DevMouse();
+		Thread key = new Thread(keyManager.keyThread);
+		Thread cam = new Thread(camera);
+		key.start();
+		cam.start();
+
+		// ball.setVisible(true);
+
+		game.addMap(new GameMap(new BackGround("maps/castle.jpg")));
+		// game.changeMap(game.getMaps().get(1));
+
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			@Override
+			public void handle(WindowEvent arg0) {
+				Platform.exit();
+				System.exit(0);
+			}
+		});
+
+		scene.setOnKeyPressed(keyManager.keyPress);
+		scene.setOnKeyReleased(keyManager.keyRelease);
+		scene.setOnMouseClicked(mouse);
+		scene.setOnMouseMoved(mouse);
+
+		MenuBar menuBar = new MenuBar();
+
+		// --- Menu File
+		Menu menuFile = new Menu("File");
+
+		Menu menuEdit = new Menu("Edit");
+		MenuItem devMode = new MenuItem("DevMode");
+		menuEdit.getItems().add(devMode);
+		devMode.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public synchronized void handle(ActionEvent event) {
+				(new PopupText(800, 200, 350, 50,
+						"PLEASE TELL ME HOW TO REVERSE DEVMODE!1!!1!",
+						primaryStage)).toggle();
+
+			}
+
+		});
+		Menu menuView = new Menu("View");
+		MenuItem zoom = new MenuItem("Zoom");
+		menuView.getItems().add(zoom);
+		menuBar.getMenus().addAll(menuFile, menuEdit, menuView);
+		final PopupText test = new PopupText("THIS IS A TEST", primaryStage);
+		Button toggle = new Button("toggleTest");
+		toggle.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public synchronized void handle(ActionEvent event) {
+				test.toggle();
+			}
+		});
+		toggle.setLayoutX(200);
+		toggle.setLayoutY(100);
+
+		((Group) scene.getRoot()).getChildren().addAll(menuBar, toggle);
+
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+		BouncyBall ball = new BouncyBall(30, 90, 8, Color.RED);
+		// Creature ball = new Creature(30,90);
+		ball.addToMap(game.getCurrentMap());
+		ball.setVisible(true);
+	}
+}
