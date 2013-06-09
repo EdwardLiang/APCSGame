fb67280dd492857707a9f1d4ce12e403222779e9
edward <Edward.k.liang@gmail.com>
2013-06-09 10:50:22 -0700

    Red is ok though.

--- src/entities/DeadlyStaticPathEntity.java
+++ src/entities/DeadlyStaticPathEntity.java
@@ -12,21 +12,30 @@ import utils.PathUtil;
 public class DeadlyStaticPathEntity extends StaticPathEntity {
 	public DeadlyStaticPathEntity(Vec2[] verts) {
 		super(verts);
-		/*Image clouds = new Image("sprites/DeadlyTexture.jpg");
-		((Polygon) node).setFill(new ImagePattern(clouds));*/
+		/*
+		 * Image clouds = new Image("sprites/DeadlyTexture.jpg"); ((Polygon)
+		 * node).setFill(new ImagePattern(clouds));
+		 */
+		((Polygon) node).setFill(Color.RED);
+
 	}
+
 	public DeadlyStaticPathEntity(Vec2[] lp, Vec2[] local, float x, float y,
 			float width, float height) {
 		super(lp, local, x, y, width, height);
-		/*Image clouds = new Image("sprites/DeadlyTexture.jpg");
-		((Polygon) node).setFill(new ImagePattern(clouds));*/
+		((Polygon) node).setFill(Color.RED);
+
+		/*
+		 * Image clouds = new Image("sprites/DeadlyTexture.jpg"); ((Polygon)
+		 * node).setFill(new ImagePattern(clouds));
+		 */
 	}
+
 	public static DeadlyStaticPathEntity parse(String[] frags) {
 		return new DeadlyStaticPathEntity(PathUtil.parseVec2(frags[3]),
 				PathUtil.parseVec2(frags[4]), Float.parseFloat(frags[1]),
 				Float.parseFloat(frags[2]), Float.parseFloat(frags[5]),
 				Float.parseFloat(frags[6]));
 	}
-
 
 }
